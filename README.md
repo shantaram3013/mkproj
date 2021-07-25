6097f02d38fb8394ffddb31a8c6710416757b2a2
Siddharth Singh <simsid66@gmail.com>
2021-07-25 14:04:12 +0530

    Add confirmation prompt and prompt mode - `mkproj` now confirms the settings you chose before committing any changes to disk. - Added the prompt mode, which is an interactive interface for creating projects.

--- README.md
+++ README.md
@@ -1,60 +1,85 @@
 # mkproj
 
-A python script to quickly generate a project directory with boilerplate for a variety of technologies. Also initialises an empty git repo.
+A python script to quickly generate a project directory with boilerplate for a
+variety of technologies. Also initialises an empty git repo.
 
 ### Currently supported targets:
-* `c` - A C project. Creates a `Makefile` and `main.c` file.
-* `deno` - A deno project. Creates a `Makefile` and `main.ts` file.
-* `html5` - An HTML5 static site.
-* `node` - Uses `npm init` to initialise your project.
-* `py` - A Python project.
-* `webapp` - An HTML5/CSS/JS webapp.
+
+- `c` - A C project. Creates a `Makefile` and `main.c` file.
+- `deno` - A deno project. Creates a `Makefile` and `main.ts` file.
+- `html5` - An HTML5 static site.
+- `node` - Uses `npm init` to initialise your project.
+- `py` - A Python project.
+- `webapp` - An HTML5/CSS/JS webapp.
 
 ### Usage:
 
 ```sh
-    mkproj [options] ProjectName Target
+mkproj [-n ProjectName] [-t Target] [other options]
 ```
 
 ### Options:
-  * `-h`, `--help`            shows a help message.
-  * `-l`, `--license`         The name of a license to use.
-  * `-r`, `--readme`          Creates a README file if this option is present.
-  * `-s`, `--shebang`         Adds a shebang for files that support it if this option is present.
-  * `-t`, `--todo`            Creates a TODO file if this option is present.
+
+- `-h`, `--help` shows a help message.
+- `-l`, `--license` The name of a license to use.
+- `-n',`--name' The name of the project. If this argument is missing mkproj will
+  launch in prompt mode.
+- `-r`, `--readme` Creates a README file if this option is present.
+- `-s`, `--shebang` Adds a shebang for files that support it if this option is
+  present.
+- `-t`, `--type` The type of project to create. If this option is missing mkproj
+  will launch in prompt mode.
+- `-T`, `--todo` Creates a TODO file if this option is present.
+
+### Prompt mode:
+
+If mkproj is launched with either no options or either the name or the type
+option missing, prompt mode will start, and prompt you for the values of the
+various options.
 
 ### Installing:
-`mkproj` is intended to be installed via a shell alias or script so that you can supply whatever arguments you prefer to create your default project with.
 
-For example, to create a bash alias that enables readme and todo creation and generates the MIT license by default:
+`mkproj` is intended to be installed via a shell alias or script so that you can
+supply whatever arguments you prefer to create your default project with.
+
+For example, to create a bash alias that enables readme and todo creation and
+generates the MIT license by default:
+
 ```bash
-    echo 'alias mkproj="/path/to/this/repo/mkproj.py -r -t -l MIT"' >> "$HOME"/.bashrc
+echo 'alias mkproj="/path/to/this/repo/mkproj.py -r -t -l MIT"' >> "$HOME"/.bashrc
 ```
 
 The same example in fish:
+
 <!--Yes, I know fish isn't bash, but syntax highlighting is nice.-->
+
 ```bash
-    alias mkproj='/path/to/this/repo/mkproj.py -r -t -l MIT'
+alias mkproj='/path/to/this/repo/mkproj.py -r -t -l MIT'
     funcsave mkproj
 ```
 
 Then you can invoke it with:
+
 ```bash
-    mkproj ProjectName Target
+mkproj ProjectName Target
 ```
 
-Alternatively, create a shell script in a location that's on your `PATH` and call mkproj within it.
+Alternatively, create a shell script in a location that's on your `PATH` and
+call mkproj within it.
 
 You could also create a symlink to it.
 
 For example, to create a symlink in `$HOME/.local/bin`:
+
 ```bash
-    ln -s /path/to/this/repo/mkproj.py "$HOME"/.local/bin
+ln -s /path/to/this/repo/mkproj.py "$HOME"/.local/bin
 ```
 
 ### Contributing:
 
-Feel free to make PRs to add licenses and project types of your own! I'll review them whenever I can.
+Feel free to make PRs to add licenses and project types of your own! I'll review
+them whenever I can.
 
 ---
+
 Copyright © 2021 Siddharth Singh
--- mkproj.py
+++ mkproj.py
@@ -12,6 +12,13 @@ from pathlib import Path
 from mkproj_licenses import licenses, year
 from mkproj_types import types
 
+confirm_string = """\
+You have chosen to make a new project with the following options:
+    * name: %s
+    * type: %s
+"""
+
+
 def delete_path(path):
     try:
         os.remove(path)
@@ -20,32 +27,34 @@ def delete_path(path):
     except PermissionError:
         die(f"Could not remove {path}: permission denied.")
 
+
 def die(*msg, exitcode=1):
     print(*msg, file=sys.stderr)
     exit(exitcode)
+
 
 def write_str_to_file(file: str, string: str):
     with open(file, "a+") as fp:
         fp.write(string)
 
+
 def write_line_to_file(file: str, string: str):
     write_str_to_file(file, string + "\n")
 
+
 def try_shell_launch():
-    init_shell = ""
-    try:
-        init_shell = input("Initialise a shell in the new dir? (yes/no) ")
-    except:
-        exit(0)
-    if init_shell in ['n', 'no']:
-        exit(0)
-    else:
+    init_shell = input_bool("Initialise a shell in the new dir? (yes/no) ")
+
+    if init_shell:
         shell = os.getenv("SHELL")
         if shell:
             subprocess.run([shell])
         else:
             print("Couldn't find your shell, exiting.")
             exit(0)
+    else:
+        exit(0)
+
 
 def get_user_input(desc):
     rval = ""
@@ -56,47 +65,121 @@ def get_user_input(desc):
             rval = None
     return rval
 
+
 def cancellable_input(prompt):
     try:
         return input(prompt)
     except:
         die("\nCanceled.")
 
+
+def input_bool(prompt):
+    try:
+        return input(prompt) in ['y', 'yes']
+    except:
+        return False
+
+
 def initialise_parser(parser):
-    parser.add_argument('name', type=str,
-                      help='Name of the directory to create.')
-    parser.add_argument('type', type=str, help='Type of the project to create.', choices=types.keys())
+    parser.add_argument('-n', '--name', type=str,
+                        help='Name of the directory to create.')
+    parser.add_argument(
+        '-t', '--type', type=str, help='Type of the project to create.', choices=types.keys())
     parser.add_argument('-r', '--readme', action='store_true',
-                      help='Creates a README file if this option is present.')
-    parser.add_argument('-t', '--todo', action='store_true',
-                      help='Creates a TODO file if this option is present.')
+                        help='Creates a README file if this option is present.')
+    parser.add_argument('-T', '--todo', action='store_true',
+                        help='Creates a TODO file if this option is present.')
     parser.add_argument('-s', '--shebang', action='store_true',
-                      help='Adds a shebang for supported files if this option is present.')
+                        help='Adds a shebang for supported files if this option is present.')
     parser.add_argument('-l', '--license', type=str,
-                      help='The name of a license to use.', choices=set(licenses.keys()))
+                        help='The name of a license to use.', choices=set(licenses.keys()))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Create a new blank project directory with necessary boilerplate given a technology and its name.',
-        prog='mkproj'
+        description='Create a new blank project directory with necessary boilerplate.',
+        prog='mkproj',
+        exit_on_error=True
     )
 
     initialise_parser(parser)
-    
     args = parser.parse_args()
-    proj_dir = os.path.join(os.getcwd(), args.name)
-    get_proj_path = lambda x: os.path.join(proj_dir, x)
 
-    if (os.path.exists(proj_dir)):
-        overwrite = cancellable_input("Cannot create project directory: a file or directory with the same name already exists. Try deleting it? (yes/no) ")
-        if (overwrite in ['y', 'yes']):
-            confirm = cancellable_input(f'Type "{args.name.replace("/", "")}" (without the quotes) to confirm deletion. THIS CANNOT BE UNDONE! ')
-            if confirm == args.name:
-                delete_path(proj_dir)
-            else:
-                die("Canceled.")
-        else:
+    if not len(sys.argv) > 1 or not args.name or not args.type:
+        name = args.name
+        if not name:
+            name = get_user_input('a name for your project')
+            if not name:
+                die('Cancelled.')
+        t = args.type
+        if not t:
+            t = get_user_input(
+                'a type from [' + ', '.join(types.keys()) + '] for your project')
+            if t not in types.keys():
+                die('Invalid type selected.')
+        readme = args.readme
+        if not readme:
+            readme = input_bool('Create a readme? (y/N) ')
+        todo = args.todo
+        if not todo:
+            todo = input_bool('Create a todo? (y/N) ')
+        shebang = args.shebang
+        if not shebang:
+            shebang = input_bool('Add shebangs to supported files? (y/N) ')
+        lic = args.license
+        if not lic:
+            create_lic = input_bool('Add a license? ')
+            if create_lic:
+                lic = get_user_input(
+                    'a license from [' + ', '.join(licenses.keys()) + '] for your project')
+                if lic not in licenses.keys():
+                    die('Invalid license selected.')
+
+        constructed_args = ['-n', name, '-t', t]
+        if readme:
+            constructed_args.append('-r')
+        if todo:
+            constructed_args.append('-T')
+        if shebang:
+            constructed_args.append('-s')
+        if lic:
+            constructed_args += ['-l', lic]
+        args = parser.parse_args(constructed_args)
+
+    args_list = []
+    if (args.todo):
+        args_list.append('Create todo')
+    if (args.readme):
+        args_list.append('Create readme')
+    if (args.license):
+        args_list.append('Create license')
+    if (args.shebang):
+        args_list.append('Add shebangs to supported files')
+
+    args_string = ''
+    if len(args_list) > 0:
+        args_string += '    * ' + '\n    * '.join(args_list)
+
+    print((confirm_string + args_string) % (args.name, args.type))
+    cnf = input_bool('Is this correct? (y/N): ')
+    if cnf:
+        pass
+    else:
+        die('Cancelled.')
+
+    proj_dir = os.path.join(os.getcwd(), args.name)
+    def get_proj_path(x): return os.path.join(proj_dir, x)
+
+    if os.path.exists(proj_dir):
+        overwrite = input_bool(
+            "Cannot create project directory: a file or directory with the same name already exists. Try deleting it? (y/N) ")
+        if not overwrite:
+            die('Cancelled.')
+        confirm = cancellable_input(
+            f'Type "{args.name.replace("/", "")}" (without the quotes) to confirm deletion. THIS CANNOT BE UNDONE! ')
+        if confirm != args.name:
             die("Canceled.")
+        delete_path(proj_dir)
 
     try:
         os.mkdir(proj_dir)
@@ -116,9 +199,9 @@ if __name__ == "__main__":
 
             if args.shebang and 'shebang' in fobj:
                 write_line_to_file(fpath, fobj['shebang'])
-            
+
             write_str_to_file(fpath, fobj['contents'])
-    
+
     if 'commands' in proj:
         for command in proj['commands']:
             subprocess.run(command)
@@ -129,8 +212,9 @@ if __name__ == "__main__":
     if args.readme:
         readme_path = get_proj_path('README.md')
         print("Creating readme...")
-        pretty_name = get_user_input("a name for your project") or "Project"
-        desc = get_user_input("a line describing your project") or "Description line."
+        pretty_name = args.name
+        desc = get_user_input(
+            "a line describing your project") or "Description line."
         write_line_to_file(readme_path, f"# {pretty_name}")
         write_line_to_file(readme_path, "")
         write_line_to_file(readme_path, f"{desc}")
@@ -145,7 +229,7 @@ if __name__ == "__main__":
             if file == "LICENSE":
                 body_str = body_str.replace("YYYY", year) % name
             write_str_to_file(get_proj_path(file), body_str)
-        
+
         print("License created. Be sure to read up on the terms of your license and add headers to each project file!")
-    
+
     try_shell_launch()
